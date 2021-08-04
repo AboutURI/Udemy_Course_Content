5b4a3d870981bbd917c2e38a6b29d8a63ba9c640
Jacob Stevens <J.stevensOne@gmail.com>
2021-08-03 23:58:21 -0400

    setup newRelic

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CourseContent from './components/CourseContent.jsx';
+
 
 ReactDOM.render(<CourseContent />, document.getElementById('content'));
